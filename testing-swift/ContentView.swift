2cf3d114a9de992f11882188ba5659ceab1ca4da
Anthony Benitez <anthony.benitezrodriguez@gmail.com>
2023-07-16 10:25:30 -0500

    Initial Commit

---
+++ testing-swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testing-swift
+//
+//  Created by Anthony Benitez-Rodriguez on 7/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testing-swift/testing_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_swiftApp.swift
+//  testing-swift
+//
+//  Created by Anthony Benitez-Rodriguez on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct testing_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
